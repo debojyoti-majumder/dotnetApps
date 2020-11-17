31dd6277c5e0669f6ca7b61248f2c1485816ca6b
Debojyoti Majumder <debojyotim@vmware.com>
2020-11-17 14:21:56 +0530

    Adding a basic console application

---
+++ basicDbApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+    To build this program clone this repository first then 
+    use "dotnet build"
+
+    To Run the application use this command "dotnet run"
+*/
+
+using System;
+
+namespace basicDbApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic data base application");
+        }
+    }
+}
